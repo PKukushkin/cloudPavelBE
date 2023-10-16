72194430e2282a4cf2fbbf644d33a6133abf92cc
PavelKukushkin <pavelkukushkin@hotmail.com>
2023-10-16 17:34:15 +0200

    Initial commit

---
+++ src/main/java/com/java/cloudPavelBE/CloudPavelBeApplication.java
@@ -0,0 +1,13 @@
+package com.java.cloudPavelBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPavelBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPavelBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/cloudPavelBE/CloudPavelBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cloudPavelBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPavelBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
